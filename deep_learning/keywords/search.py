1163c04d30ba63e0ff70c85630d8f0e74453407c
apdif <apcm01mexico@outlook.com>
2018-02-01 11:01:35 -0600

    implement in this project nltk

--- deep_learning/keywords/search.py
+++ deep_learning/keywords/search.py
@@ -1,11 +1,13 @@
 # -*- coding: UTF-8 -*-
 from deep_learning.keywords import lib_error
-import time, conn, langdetect, sys
+import time, conn, langdetect, re, sys
+from ast import literal_eval
 from multiprocessing import Process, Lock   
+
 from bs4 import BeautifulSoup
 import nltk
 from nltk.corpus import stopwords
-
+from nltk.stem import WordNetLemmatizer
 
 def identify(html): 
     txttodb = 'Untitled'
@@ -26,28 +28,51 @@ def identify(html):
             possiblelangs = 'NA'
         return txttodb,detectlang,possiblelangs 
  
+def delete_chr(text,str):
+    while text.find(str) >= 0:
+        text = text.replace(str,' ')
+    return text
+
+
 def get_keyword(j,p):
-    keywods = ['mp3','descargar','download','music','convert','free','gratis']  
-    print identify(p)    
+    lemmatizer = WordNetLemmatizer()
+    #Identify lenguage
+    print identify(p) 
+    #extract only the content between the tag <body> and </body>   
     start = p.find("<body")
     end = p.find('/body>',start)
-    newpage = p[start:end]
-    
-    soup = BeautifulSoup(newpage,"html5lib")
-    text = soup.get_text(strip=True).replace('\n',' ').replace('\t',' ')
-    while text.find('  ') >= 0:
-        text = text.replace('  ',' ')
-    
-    tokens = [t for t in text.split()]
-    clean_tokens = tokens[:]    
-    sr = stopwords.words('english')
-    for token in tokens:
-        if token in stopwords.words('english'):
+    #delete all the tag of html
+    soup = BeautifulSoup(p[start:end + 6].replace('>','>*'),"html5lib")
+    text = soup.get_text(strip=True)
+    #find and delete the special characters
+    chr_esp = ['*','\n','\t','.',',','\'','_','\"',"-",'  ']
+    for n in chr_esp:
+        text = delete_chr(text,n)
+    #split the text
+    tokens = [t for t in text.split()] 
+    #delete words with special characters
+    patron = re.compile(r"^([a-zA-Z]+)$")   #[a-zA-Z] OR \w
+    new_tokens = []
+    delete_tokens = []
+    for i in tokens:
+        if patron.search(i):
+            new_tokens.append(lemmatizer.lemmatize(i, pos="v"))
+        else:
+            delete_tokens.append(i)
+    #clean 
+    clean_tokens = new_tokens[:]    
+    for token in new_tokens:
+        if token in stopwords.words():
             clean_tokens.remove(token) 
+    #create the frequence        
     freq = nltk.FreqDist(clean_tokens)
-    freq.plot(20,cumulative=False)
-    sys.exit()
-    
+    print freq.most_common(len(freq))
+    print "-------------------------------------------------------------------"
+    time.sleep(1)
+    #create a graph
+    #freq.plot(20,cumulative=False) 
+    #Search keywords
+    #keywods = ['mp3','descargar','download','music','convert','free','gratis']
 
 def find_all_keywords(i,p,n,m):  
     website = p
